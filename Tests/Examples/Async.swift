d5bf21c73785b08806b6c9702e9e365e0bb9d50e
Jackson Utsch <jutechs@gmail.com>
2022-05-05 07:15:49 -0700

    Remove unused localization, update test availability

--- Tests/Examples/Async.swift
+++ Tests/Examples/Async.swift
@@ -3,7 +3,7 @@ import XCTest
 @testable import HTTP
 
 final class AsyncTest: XCTestCase {
-  @available(macOS 12.0, *)
+  @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
   func testMakeRequest() async {
     do {
       let result = try await HTTP.async(
