a2bc2604044ef5caa7c7873da02ce5c5f17e6c42
Jackson Utsch <jutechs@gmail.com>
2023-01-20 14:37:06 -0700

    tvOS fix

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/JacksonUtsch/URLSessionBackport.git", .upToNextMajor(from: "0.1.0")),
+    .package(url: "https://github.com/JacksonUtsch/URLSessionBackport.git", .upToNextMajor(from: "0.2.1")),
   ],
   targets: [
     .target(
